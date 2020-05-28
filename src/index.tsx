0b3003f29debf603ae52e94e6ce0bad3d4138ad4
Sajankumar Vijayan <sajankumarvijayan@Sajankumars-MacBook-Air.local>
2020-05-28 18:23:57 +0200

    Updates

--- src/index.tsx
+++
@@ -1,8 +0,0 @@
-import './index.css';
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import LayoutContainer from "./components/Layout";
-
-ReactDOM.render(<LayoutContainer />, document.querySelector("#root"));
--- src/service/api.ts
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const api = axios.create({
-  baseURL: 'https://dog.ceo/api',
-  timeout: 0,
-});
